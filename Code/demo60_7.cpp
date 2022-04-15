19b483b12e26053832fa6c3d29ed30b26eb8a3f3
Patrick <83408755+CoderBaii@users.noreply.github.com>
2022-04-15 23:13:41 +0800

    C

--- Code/demo60_7.cpp
+++ Code/demo60_7.cpp
@@ -25,3 +25,46 @@ int main()
     printf("\n");
     return 0;
 }
+
+/*
+#include <stdio.h>
+//冒泡排序
+
+void Bubble_sort(int a[],int sz)
+{
+    int i = 0;
+    for(i=0;i<sz-1;i++)
+    {
+        int j = 0;
+        int flag = 1; //假设已经有序
+        for(j=0;j<sz-1-i;j++)
+        {
+            if(a[j] > a[j+1]) //相邻的两个元素比较
+            {
+                int tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = tmp;
+                flag = 0; //如果进入循环说明至少交换了一次，所以flag为假
+            }
+        }
+        if(flag == 1) //如果数组已经有序，就不进入if,flag为真直接跳出循环
+        {
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int arr[10] = {1,2,4,3,5,6,7,8,9,10};
+    int sz = sizeof(arr)/sizeof(arr[0]);
+    Bubble_sort(arr,sz);
+
+    int i = 0;
+    for(i=0;i<sz;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+}
+*/
